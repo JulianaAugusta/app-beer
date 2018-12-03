1dbd5aada265c0f81c874e200ff41e5fe14d8eb3
JulianaAugusta <juli_aug@hotmail.com>
2018-12-03 21:57:36 -0200

    Criando um componente

--- src/app/lista-cerveja/lista-cerveja.component.ts
+++ src/app/lista-cerveja/lista-cerveja.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCervejaComponent implements OnInit
 {
-  titulo: string = 'Lista de Cervejas:'
-  constructor() { }
+  titulo: string = ''
 
-  ngOnInit() {
+  constructor() { }//só instacia
+
+  ngOnInit() {//instancia e faz
+    this.titulo = 'Cervejas'
   }
 
+  OnAlert(nome: string){
+    alert(nome)
+    this.titulo = nome
+  }
 }
